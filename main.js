f111278f1e81488d5f64eec81af5e3dd2c249975
Evgen21-rgb <evges2180@gmail.com>
2026-02-15 22:32:18 +0300

    Операторы &&-И, ||-или

--- main.js
+++ main.js
@@ -1,13 +1,63 @@
-let sum = +prompt("Укажите стартовую сумму", 1000 );
-const persent = 15;
-const result =2000;
 
-let count = 0;
+const location_1 = 3;
+const location_2 = 4;
+const location_3 = 5;
 
-const depositTerm = +prompt("Укажите срок депозита" , 10) 
+const guess = 4;
+// if (guess === location_1 || guess === location_2 || guess === location_3) {
+//     alert("HIT!")
+// } else {
+//     alert("MISS!")
+// }
 
-while (count < depositTerm) {
-sum = sum + sum * (persent / 100)
-count = count + 1
-document.write(count + ": " + sum + "<br>")
+// if(guess === location_1){
+//     alert("HIT!")
+// } else if (guess === location_2){
+//     alert("HIT!")
+// } else if (guess === location_3){
+//     alert("HIT!")
+// } else {
+//     alert("MISS!")
+// }
+
+
+const inStock = true;
+const onSale = true;
+
+// if(inStock === true){
+//     if(onSale === true){
+//         alert("Buy!!!")
+//     }
+// }
+
+// if(inStock ===true && onSale === true){
+//     alert("Buy!!!")
+// }
+
+const price = 100;
+
+if(inStock === true && (onSale === true || price < 80)){
+    alert("Buy!!!");
 }
+
+
+
+
+
+
+
+// let sum = +prompt("Укажите стартовую сумму", 1000 );
+// const persent = 15;
+// const result =2000;
+
+// let count = 0;
+
+// const depositTerm = +prompt("Укажите срок депозита" , 10) 
+
+// while (count < depositTerm) {
+// sum = sum + sum * (persent / 100)
+// count = count + 1
+// document.write(count + ": " + sum + "<br>")
+// }
+
+
